51af1dc6f2a45dc4a91815c468abe2ca0443da7d
Holence <Holence08@gmail.com>
2022-04-02 15:54:47 +0800

    version update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app=DTAPP(sys.argv)
 
 app.setWindowIcon(DTIcon.HoloIcon1())
 app.setApplicationName("DongliTeahouseStudio")
-app.setApplicationVersion("2.0.2.3 build 220328")
+app.setApplicationVersion("2.0.2.3 build with DTPySide 0.1.4a1")
 app.setAuthor("鍵山狐")
 app.setLoginEnable(True)
 app.loadTranslation(translation)
